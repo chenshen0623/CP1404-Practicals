21b0639f5c4f173b053af9a19b1002dfe4bd994e
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-10 18:13:37 +0800

    This is my answer for guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -20,4 +20,3 @@ for i, guitar in enumerate(guitars, 1):
     vintage_string = " (vintage)" if guitar.is_vintage() else ""
     print(f"Guitar {i}: {guitar.name} ({guitar.year}), "
             f"worth ${guitar.cost:,.2f}{vintage_string}")
-
