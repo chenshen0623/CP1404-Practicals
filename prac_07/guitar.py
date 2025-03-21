2a9a97c2e417f226a3c4e67538a0395bcf5b96aa
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-21 15:39:33 +0800

    Add string representation __str__

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -9,3 +9,7 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the guitar."""
+        return f'{self.name} ({self.year}) : ${self.cost:,.2f}'
