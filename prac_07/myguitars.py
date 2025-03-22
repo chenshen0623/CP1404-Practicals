2010e7a80698226070aa9a2979aaaf0cb357344e
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-22 15:43:27 +0800

    Save guitars to new file.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -14,3 +14,37 @@ def main():
     for guitar in guitars:
         print(guitar)
     save_guitars(FILENAME, guitars)
+
+
+def load_guitars(filename):
+    """Load existing guitars' information from file."""
+    guitars = []
+    with open(filename, 'r', newline='') as in_file:
+        for line in in_file:
+            part = line.strip().split(',')
+            guitar = Guitar(part[0], int(part[1]), float(part[2]))
+            guitars.append(guitar)
+    return guitars
+
+
+def get_guitars():
+    """User enters the new guitar and saves it to the list"""
+    guitars = []
+    guitar_name = input("Name: ")
+    while guitar_name != '':
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
+        guitar = Guitar(guitar_name, guitar_year, guitar_cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        guitar_name = input("Name: ")
+    return guitars
+
+def save_guitars(filename, guitars):
+    """Save guitars to new file."""
+    with open(filename, 'w', newline='') as out_file:
+        for guitar in guitars:
+            out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+
+
+main()
