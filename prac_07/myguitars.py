6d34b22cf31b6fd21bad4e9e1d217a78eae6716c
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-22 15:33:06 +0800

    Complete first part of More Guitars exercise

---
+++ prac_07/myguitars.py
@@ -0,0 +1,11 @@
+from prac_07.guitar import Guitar
+
+FILENAME = 'guitars.csv'
+
+
+def main():
+    """Load existing guitars and get new guitars, display all guitars in order, then save to file."""
+    guitars = load_guitars(FILENAME)
+    new_guitars = get_guitars()
+    guitars += new_guitars
+    guitars.sort()
