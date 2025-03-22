68c71d94aacbe13e93f91a5a3f640fda55bc4a61
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-22 15:54:02 +0800

    Initialize Project object.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,2 +1,10 @@
 class Project:
     """Create a project class"""
+
+    def __init__(self, name='', start_date='', priority=0, cost_estimate=0.0, completion_percentage=0):
+        """Initialize Project object."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
