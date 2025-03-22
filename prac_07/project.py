9096134ba8c78c31a4736e78329b2af6b814b45d
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-22 16:12:08 +0800

    Update the completion percentage and priority of project.

--- prac_07/project.py
+++ prac_07/project.py
@@ -17,3 +17,15 @@ class Project:
     def __lt__(self, other):
         """Add a comparison method (sorted by priority)"""
         return self.priority < other.priority
+
+    def is_completed(self):
+        """Check if project is fully done."""
+        return self.completion_percentage == 100
+
+    def update_percentage(self, percentage):
+        """Update the completion percentage of project."""
+        self.completion_percentage = percentage
+
+    def update_priority(self, priority):
+        """Update the priority of project."""
+        self.priority = priority
