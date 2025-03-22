c0ce3113b09b671e6baeb03a91ba640b6010aae5
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-22 15:58:53 +0800

    Add a comparison method (sorted by priority)

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,12 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return a string representation."""
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
+
+    def __lt__(self, other):
+        """Add a comparison method (sorted by priority)"""
+        return self.priority < other.priority
