ce13a9d683f3e4ff8edbbf0fde62d4c62eebd01b
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-03-27 18:59:47 +0800

    Complete BoxLayoutDemo

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -5,3 +5,20 @@ from kivy.lang import Builder
 class BoxLayoutDemo(App):
     """Main program - Kivy app to demo box layout."""
 
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        """Display a greeting message."""
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def clear_text(self):
+        """Clear texts of input box and output label."""
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
+
+BoxLayoutDemo().run()
