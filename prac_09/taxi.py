cd5f1a85dcf7c8a90d11f5656ac72da0e2158dc4
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-04-05 16:35:22 +0800

    Add the class variable price_per_km

---
+++ prac_09/taxi.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from prac_09.car import Car
+
+
+class Taxi(Car):
+    """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
+
