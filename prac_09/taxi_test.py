1c33ed25862874dabd74fff3cd6429d7beed348d
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-04-05 17:04:46 +0800

    Drive the taxi 40 km Print the taxi's details and the current fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,3 +2,8 @@ from prac_09.taxi import Taxi
 
 # Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
 my_taxi = Taxi('Prius 1', 100)
+# Drive the taxi 40 km
+my_taxi.drive(40)
+# Print the taxi's details and the current fare
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare()}")
