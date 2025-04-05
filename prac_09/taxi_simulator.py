d15766e3ef780f031734ef6f75565db83a07f9bb
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-04-05 19:42:44 +0800

    Update the choice function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -4,4 +4,30 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 print("Let's Drive!")
 MENU = "q)uit, c)hoose taxi, d)rive"
 
+def main():
+    """The main program of taxi simulator."""
+
+    # Welcome message
+    taxis = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    bill = 0
+    current_taxi = None
+
+    choice = input(f"{MENU}\n>>>").lower()
+    while choice != "q":
+        if choice == "c":
+            current_taxi = choose_taxi(taxis)
+        elif choice == "d":
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive.")
+            else:
+                bill += drive_taxi(current_taxi)
+                print(f"Bill to date: ${bill:.2f}")
+        else:
+
+            print("Invalid option")
+        choice = input(f"{MENU}\n>>>").lower()
 
