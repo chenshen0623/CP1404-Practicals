2c503951d95261f5fcd0fc58ce2667afca51a56e
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-04-05 16:38:38 +0800

    Change the __init__ function

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -9,3 +9,8 @@ class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
     price_per_km = 1.23
 
+    def __init__(self, name, fuel, price_per_km):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+
