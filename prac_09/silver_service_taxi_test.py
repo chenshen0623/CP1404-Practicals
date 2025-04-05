856196c9e5fd21e74f421513f84ffe4205840fb3
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-04-05 19:07:05 +0800

    modify silver_service_taxi_test.py.py

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -5,8 +5,10 @@ def main():
 
     # Set the name, fuel and fanciness
     hummer = SilverServiceTaxi("Hummer", 200, 2)
+    hummer.start_fare()
     hummer.drive(18)
     print(hummer)
+    print(f"Total fare: ${hummer.get_fare():.2f}")  # 明确输出车费
 
 if __name__ == '__main__':
     main()
