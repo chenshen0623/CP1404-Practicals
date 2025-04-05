303591e7fb466d43f59537483b81562791774df1
chenshen0623 <shen.chen@my.jcu.edu.au>
2025-04-05 17:05:28 +0800

    Restart the meter (start a new fare) and then drive the car 100 km Print the details and the current fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,3 +7,10 @@ my_taxi.drive(40)
 # Print the taxi's details and the current fare
 print(my_taxi)
 print(f"Current fare: ${my_taxi.get_fare()}")
+# Restart the meter (start a new fare) and then drive the car 100 km
+my_taxi.start_fare()
+my_taxi.add_fuel(100)
+my_taxi.drive(100)
+# Print the details and the current fare
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare()}")
